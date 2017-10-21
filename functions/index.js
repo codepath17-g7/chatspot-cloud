28aebe3d0665e544914450ee994fbcd835f32190
Syed Hakeem Abbas <hakeemsyd@fb.com>
2017-10-21 03:47:08 -0700

    append user to chatroom's users properly

--- functions/index.js
+++ functions/index.js
@@ -9,12 +9,11 @@ exports.userLocationUpdate = functions.database.ref('/users/{userId}/location')
     .onWrite(event => {
       const userCord = event.data.val();
       const uid = event.params.userId;
-      console.log('userId: ', uid);
 
       admin.database().ref('/chatrooms').once("value", function(data) {
           var rooms = data.val();
           var maxDist = 1000000; // some very very large number
-          var roomToJoin = nil;
+          var roomToJoin = "";
           _.map(rooms, function(roomObj, roomId) {
               var dist = geodist({lat:userCord.lat , lon: userCord.long},
                                  {lat:roomObj.latitude, lon: roomObj.longitude},
@@ -22,15 +21,16 @@ exports.userLocationUpdate = functions.database.ref('/users/{userId}/location')
 
               if(dist < maxDist) {
                   maxDist = dist;
-                  roomsToJoin = roomId;
+                  roomToJoin = roomId;
               }
 
               console.log('distance: ', roomId, dist);
           });
 
-          if(roomToJoin != nil) {
+          if(roomToJoin !== "") {
               console.log('joining room: ', roomToJoin);
-              admin.database().ref('/chatrooms' + roomToJoin).child('users').set({uid: true});
+              console.log('userId: ', uid);
+              admin.database().ref('/chatrooms/' + roomToJoin).child('users').child(uid).set(true);
           }
       });
 
