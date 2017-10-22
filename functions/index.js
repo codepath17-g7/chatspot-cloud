1d956fadae3cf2304fc66ec013aed2f0a5b720be
Phuong Nguyen <phuong@itsoninc.com>
2017-10-21 17:45:33 -0700

    Promise chaining

--- functions/index.js
+++ functions/index.js
@@ -6,47 +6,63 @@ var geodist = require('geodist')
 admin.initializeApp(functions.config().firebase);
 
 exports.userLocationUpdate = functions.database.ref('/users/{userId}/location')
-    .onWrite(event => {
-      const userCord = event.data.val();
-      const uid = event.params.userId;
-      const dataBase = admin.database()
+.onWrite(event => {
+    const dataBase = admin.database()
+    const userCord = event.data.val();
+    const uid = event.params.userId;
+    console.log('userId: ', uid);
 
-      admin.database().ref('/chatrooms').once("value", function(data) {
-          var rooms = data.val();
-          var maxDist = 1000000; // some very very large number
-          var roomToJoin = "";
-          _.map(rooms, function(roomObj, roomId) {
-              var dist = geodist({lat:userCord.lat , lon: userCord.long},
-                                 {lat:roomObj.latitude, lon: roomObj.longitude},
-                                 {unit: 'mi'});
+    return admin.database().ref('/chatrooms').once("value").then(function(data) {
+        var rooms = data.val();
+        var maxDist = 1000000; // some very very large number
+        var roomToJoin = "";
+        _.map(rooms, function(roomObj, roomId) {
+            var dist = geodist({lat:userCord.lat , lon: userCord.long},
+                {lat:roomObj.latitude, lon: roomObj.longitude},
+                {unit: 'mi'});
 
-              if(dist < maxDist) {
-                  maxDist = dist;
-                  roomToJoin = roomId;
-              }
+                if(dist < maxDist) {
+                    maxDist = dist;
+                    roomToJoin = roomId;
+                }
 
-              console.log('distance: ', roomId, dist);
-          });
+                console.log('distance: ', roomId, dist);
+            });
 
-          console.log('userId: ', uid);
+            if(roomToJoin !== "") {
 
-          if(roomToJoin !== "") {
-              const aroundMeRef = dataBase.ref('/users/' + uid +'/aroundMe')
-              aroundMeRef.once("value", function(data) {
-                   roomToLeave = data.val()
-				    if(roomToLeave != "") {
-					    console.log('Leavinglocal room: ', roomToLeave);
+                const aroundMeRef = dataBase.ref('/users/' + uid +'/aroundMe')
+                aroundMeRef.once("value").then(function(data) {
+                    var leaveRoomPromise = false;
+
+                    roomToLeave = data.val()
+                    if(roomToLeave != "") {
+                        console.log('Leaving local room: ', roomToLeave);
                         nodeToRemove = dataBase.ref('/chatrooms/' + roomToLeave + '/localUsers').child(uid)
                         const updates = {}
                         updates[nodeToRemove.key] = null
-                        nodeToRemove.parent.update(updates)
-					}
-					console.log('Joining local room: ', roomToJoin);
-                    dataBase.ref('/chatrooms/' + roomToJoin + '/localUsers').child(uid).set(true)
-                    aroundMeRef.set(roomToJoin)
-              })
-          }
-      });
+                        leaveRoom = nodeToRemove.parent.update(updates)
+                    }
 
-      return event.data.ref.parent.child('location').set(event.data.val());
+                    console.log('Joining local room: ', roomToJoin);
+                    dataBase.ref('/chatrooms/' + roomToJoin + '/localUsers').child(uid).set(true)
+                    joinRoomPromise = aroundMeRef.set(roomToJoin)
+
+                    return Promise.all([leaveRoomPromise, joinRoomPromise])
+                })
+            }
+        });
+        // return event.data.ref.parent.child('location').set(event.data.val());
+    });
+
+exports.newMessage = functions.database.ref('/messages/{roomId}')
+.onCreate(event => {
+    const message = event.data.val();
+    console.log('roomId: ', roomId);
+    console.log('message: ', message);
+    console.log('ref: ', event.data.ref);
+
+    // write message to around me stream of other localUsers in that room
+
+    //return event.data.ref.parent.child(roomId).set(event.data.val());
 });
