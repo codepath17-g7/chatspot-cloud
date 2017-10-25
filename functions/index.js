c8b8bb594dd8e2564ca91e3b612de0f4751cfbff
Phuong Nguyen <phuong@itsoninc.com>
2017-10-24 23:54:42 -0700

    Add logging system message for room switching

--- functions/index.js
+++ functions/index.js
@@ -15,7 +15,8 @@ exports.userLocationUpdate = functions.database.ref('/users/{userId}/location')
     return dataBase.ref('/chatrooms').once("value").then(function(data) {
         var rooms = data.val();
         var maxDist = 1000000; // some very very large number
-        var roomToJoin = "";
+        var roomGuidToJoin = "";
+        var roomToJoin;
         _.map(rooms, function(roomObj, roomId) {
             var dist = geodist({lat:userCord.lat , lon: userCord.long},
                 {lat:roomObj.latitude, lon: roomObj.longitude},
@@ -23,13 +24,14 @@ exports.userLocationUpdate = functions.database.ref('/users/{userId}/location')
 
                 if(dist < maxDist) {
                     maxDist = dist;
-                    roomToJoin = roomId;
+                    roomGuidToJoin = roomId;
+                    roomToJoin = roomObj
                 }
 
                 console.log('distance: ', roomId, dist);
             });
 
-            if(roomToJoin !== "") {
+            if(roomGuidToJoin !== "") {
 
                 const aroundMeRef = dataBase.ref('/users/' + uid +'/aroundMe')
                 aroundMeRef.once("value").then(function(data) {
@@ -44,11 +46,19 @@ exports.userLocationUpdate = functions.database.ref('/users/{userId}/location')
                         leaveRoom = nodeToRemove.parent.update(updates)
                     }
 
-                    console.log('Joining local room: ', roomToJoin);
-                    dataBase.ref('/chatrooms/' + roomToJoin + '/localUsers').child(uid).set(true)
-                    joinRoomPromise = aroundMeRef.set(roomToJoin)
+                    console.log('Joining local room: ', roomGuidToJoin, roomToJoin.name);
+                    dataBase.ref('/chatrooms/' + roomGuidToJoin + '/localUsers').child(uid).set(true)
+                    joinRoomPromise = aroundMeRef.set(roomGuidToJoin)
 
-                    return Promise.all([leaveRoomPromise, joinRoomPromise])
+
+                    console.log('Writing system message', roomToJoin.name);
+                    var newMessageRef = dataBase.ref('aroundme/'+uid).push()
+                    systemMessagePromise = newMessageRef.set ({
+                        message: "You are now in " + roomToJoin.name,
+                        system: true
+                    })
+
+                    return Promise.all([leaveRoomPromise, joinRoomPromise, systemMessagePromise])
                 })
             }
         });
@@ -67,18 +77,17 @@ exports.newMessage = functions.database.ref('/messages/{roomId}/{messageId}')
 
     // write message to around me stream of other localUsers in that room
     return dataBase.ref('/chatrooms/'+roomId+'/localUsers').once("value").then(function(localUsers) {
-        const updates = []
         console.log('localUsers: ', localUsers.val());
         if (!localUsers.exists() || localUsers.numChildren()==0) {
             console.log('No local users')
             return;
         }
 
+        const updates = []
         localUsers.forEach(function(localUser){
             console.log('Adding to : ', localUser.key);
             updates.push(dataBase.ref('aroundme/'+localUser.key).child(messageId).set(message))
-            return Promise.all(updates)
         });
-
+        return Promise.all(updates)
     });
 });
